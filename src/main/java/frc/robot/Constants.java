49502e112e7ab26c8b6bb8a6fd1030de66c3c870
frcteam2791 <team2791@gmail.com>
2022-01-26 17:37:17 -0800

    characterization constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,16 +30,16 @@ public final class Constants {
 	public static final double kRamseteB = 2;
     public static final double kRamseteZeta = .7;
     
-	public static final double ksVolts = 0;
-	public static final double kvVoltSecondsPerMeter = 0;
-    public static final double kaVoltSecondsSquaredPerMeter = 0;
+	public static final double ksVolts = 0.087396;
+	public static final double kvVoltSecondsPerMeter = 2.3279;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.25637;
 
     private static final double kTrackwidthMeters = 0;
     
     public static final DifferentialDriveKinematics kDriveKinematics =
         new DifferentialDriveKinematics(kTrackwidthMeters);
     
-	public static final double kPDriveVel = 0;
+	public static final double kPDriveVel = 0.23188;
 
 	public static final double kMaxSpeedMetersPerSecond = 3;
 	public static final double kMaxAccelerationMetersPerSecondSquared = 1;
