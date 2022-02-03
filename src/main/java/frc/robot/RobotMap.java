f470763f69a50d5d5a2e0423c2010ffc99391df8
frcteam2791 <team2791@gmail.com>
2022-02-02 16:17:07 -0800

    indexer

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,6 +5,8 @@ public class RobotMap {
     public static final int rightLeaderID = 21;
     public static final int leftFollowerID = 20;
     public static final int rightFollowerID = 19;
+    public static final int lowerindexerID = 68;
+    public static final int upperindexerID = 67;
 
 
 }
---
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+public class Indexer extends SubsystemBase {
+  private CANSparkMax lowerMotor; 
+  private CANSparkMax upperMotor;
+  /** Creates a new Indexer. */
+  public Indexer() {
+      lowerMotor = new CANSparkMax(RobotMap.lowerindexerID, MotorType.kBrushless);
+      upperMotor = new CANSparkMax(RobotMap.upperindexerID, MotorType.kBrushless);
+  }
+
+
+  public void setLowerMotor(double lowerSpeed) {
+      lowerMotor.set(lowerSpeed);
+  }
+  public void setUpperMotor(double upperSpeed) {
+      upperMotor.set(upperSpeed);
+  }
+  public void stopLowerMotor() {
+      lowerMotor.set(0);
+  }
+  public void stopUpperMotor() {
+      upperMotor.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
