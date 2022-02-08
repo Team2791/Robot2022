55fc8e1a61eed9a258d652bde1a1b106b9a1c8f5
jmsung730 <58711472+jmsung730@users.noreply.github.com>
2022-02-08 08:54:16 -0500

    delete

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -4,43 +4,20 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Indexer extends SubsystemBase {
-  private CANSparkMax lowerMotor; 
-  private CANSparkMax upperMotor;
-  DigitalInput limitSwitch;
+
   /** Creates a new Indexer. */
 
   public Indexer() {
-      lowerMotor = new CANSparkMax(RobotMap.lowerindexerID, MotorType.kBrushless);
-      upperMotor = new CANSparkMax(RobotMap.upperindexerID, MotorType.kBrushless);
-      limitSwitch = new DigitalInput(RobotMap.limitSwitch);
+
   }
 
-  public boolean getLimitSwitch() {
-        if(limitSwitch.get()) {
-            return true; 
-        }
-        return false;
-  }
-  public void setLowerMotor(double lowerSpeed) {
-      lowerMotor.set(lowerSpeed);
-  }
-  public void setUpperMotor(double upperSpeed) {
-      upperMotor.set(upperSpeed);
-  }
-  public void stopLowerMotor() {
-      lowerMotor.set(0);
-  }
-  public void stopUpperMotor() {
-      upperMotor.set(0);
-  }
+ 
 
   @Override
   public void periodic() {
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,36 +4,23 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Shooter extends SubsystemBase {
-  private CANSparkMax flyWheelOne, flyWheelTwo;
   public Shooter() {
-    flyWheelOne = new CANSparkMax(RobotMap.flyWheelOneID, MotorType.kBrushless);
-    flyWheelTwo = new CANSparkMax(RobotMap.flyWheelTwoID, MotorType.kBrushless);
+   
 
   }
 
-  public void setFlyWheelOne(double speed){
-    flyWheelOne.set(speed);
-  }
-  public void setFlyWheelTwo(double speed){
-    flyWheelTwo.set(speed);
-  }
 
   
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Fly Wheel 1 Velocity", flyWheelOne.get());
-    SmartDashboard.putNumber("Fly Wheel 2 Velocity", flyWheelTwo.get());
+
   }
 }
