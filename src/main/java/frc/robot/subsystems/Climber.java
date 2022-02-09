3472096cea6a888b2d0dfa76b5eb870dedcd7711
Eli <edgach12@gmail.com>
2022-02-09 18:39:48 -0500

    Climber subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,23 +7,48 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
-  private CANSparkMax LCM,RCM;
+  private CANSparkMax climbLeft,climbRight;
+  private Solenoid climbSolRight, climbSolLeft;
+
 
   public Climber() {
-    LCM = new CANSparkMax(RobotMap.rightClimbID, MotorType.kBrushless);
-    RCM = new CANSparkMax(RobotMap.rightClimbID, MotorType.kBrushless);
-  }
+    climbLeft = new CANSparkMax(RobotMap.rightClimbID, MotorType.kBrushless);
+    climbRight = new CANSparkMax(RobotMap.rightClimbID, MotorType.kBrushless);
+    climbSolLeft = new Solenoid(PneumaticsModuleType.CTREPCM, RobotMap.climb);
+    climbSolRight = new Solenoid(PneumaticsModuleType.CTREPCM, RobotMap.climb);
 
+
+     
+  }
+  public void setMotors(double left, double right)
+  {
+    climbLeft.set(left);
+    climbRight.set(right);
+
+  }
+  public void setExtended()
+  {
+    climbSolRight.set(true);
+    climbSolLeft.set(true);
+  }
+  public void setRetracted()
+  {
+    climbSolRight.set(false);
+    climbSolLeft.set(false);
+
+  }
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Right Velocity", LCM.get());
-    SmartDashboard.putNumber("Left Velocity", RCM.get());
+    SmartDashboard.putNumber("Right Velocity", climbRight.get());
+    SmartDashboard.putNumber("Left Velocity", climbLeft.get());
     // This method will be called once per scheduler run
   }
 }
