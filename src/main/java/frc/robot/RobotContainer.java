79041e0b4826fdd4856ca815f0a9ebca0ad6c4b4
frcteam2791 <team2791@gmail.com>
2022-02-16 18:27:46 -0800

    changed trajectory

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -99,10 +99,10 @@ public class RobotContainer {
             new Pose2d(0, 0, new Rotation2d(0)),
             // Pass through these two interior waypoints, making an 's' curve path
             //List.of(new Translation2d(1, 1), new Translation2d(2,-1)),
-            List.of(),
+            List.of(new Translation2d(2, 0)),
 
             // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(2, 2, new Rotation2d(0)),
+            new Pose2d(4, 0, new Rotation2d(0)),
             // Pass config
             config);
 
