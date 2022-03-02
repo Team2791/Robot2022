fef868ff6fa82fc1d19982f69b5db39833da7ae3
frcteam2791 <team2791@gmail.com>
2022-03-01 16:44:11 -0800

    z

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,9 +24,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
- import frc.robot.subsystems.Indexer;
- import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Intake;
+import frc.robot.commands.AutoCommandGroups.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
