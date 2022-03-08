9362cc4f04c677eaff6b6ba24ba0d3972a7acf7f
jmsung730 <58711472+jmsung730@users.noreply.github.com>
2022-03-07 22:44:04 -0500

    organizing

--- src/main/java/frc/robot/commands/AutoCommands/AutoMidShot.java
+++ src/main/java/frc/robot/commands/AutoCommands/AutoMidShot.java
@@ -2,30 +2,23 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.IntakeCommands;
+package frc.robot.commands.AutoCommands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.Robot;
 
-public class RunIntake extends CommandBase {
-  /** Creates a new RunIntake. */
-  public RunIntake() {
+public class AutoMidShot extends CommandBase {
+  /** Creates a new AutoMidShot. */
+  public AutoMidShot() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(Robot.intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    Robot.intake.setMotor(Constants.intakeSpeed);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
@@ -34,6 +27,6 @@ public class RunIntake extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return false;
   }
 }
