1345114f2dacc347c925e4a378813d82d5c0ecfe
jmsung730 <58711472+jmsung730@users.noreply.github.com>
2022-10-19 19:44:40 -0400

    joe

--- src/main/java/frc/robot/commands/AutoCommands/DriveAndIndex34.java
+++ src/main/java/frc/robot/commands/AutoCommands/DriveAndIndex34.java
@@ -1,32 +1,19 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.AutoCommands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import frc.robot.commands.IntakeCommands.ExtendRunIntake;
 
-public class DriveAndIndex34 extends CommandBase {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class DriveAndIndex34 extends ParallelDeadlineGroup {
   /** Creates a new DriveAndIndex34. */
-  public DriveAndIndex34() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+  public DriveAndIndex34(double left, double right, double speed) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    super(new DriveDistanceBack(left, right, speed), new Indexer34(), new ExtendRunIntakeTimer());
+    //setDeadline(new DriveDistanceBack(left, right, speed));
+    //addCommands(new DriveDistanceBack(left, right, speed), new Indexer34(), new ExtendRunIntakeTimer());
   }
 }
