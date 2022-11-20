9a84d0e9dc3dcb05ede35ea20a40548f21ca01d5
jmsung730 <58711472+jmsung730@users.noreply.github.com>
2022-11-20 12:17:40 -0500

    clean up

--- src/main/java/frc/robot/subsystem/Shooter.java
+++ src/main/java/frc/robot/subsystem/Shooter.java
@@ -2,11 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystem;
+package frc.robot.Subsystem;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
@@ -14,8 +14,8 @@ public class Shooter extends SubsystemBase {
 	private final CANSparkMax right;
 
 	public Shooter() {
-		left = new CANSparkMax(7, MotorType.kBrushless);
-		right = new CANSparkMax(8, MotorType.kBrushless);
+		left = new CANSparkMax(RobotMap.flyWheelBackID, MotorType.kBrushless);
+		right = new CANSparkMax(RobotMap.flyWheelFrontID, MotorType.kBrushless);
 	}
 
 	public void setSpeedL(double s) {
