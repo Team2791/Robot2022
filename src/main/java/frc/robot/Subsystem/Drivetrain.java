119bdb54da2e129133bc486ed4a19509c864195a
jmsung730 <58711472+jmsung730@users.noreply.github.com>
2022-11-20 12:13:41 -0500

    clearn

--- src/main/java/frc/robot/Subsystem/Drivetrain.java
+++ src/main/java/frc/robot/Subsystem/Drivetrain.java
@@ -1,17 +1,88 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.Subsystem;
 
+import frc.robot.*;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Drivetrain extends SubsystemBase {
-  /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+public class Drivetrain extends SubsystemBase{
+    private CANSparkMax leftLeader, rightLeader, leftFollower, rightFollower;
+    private RelativeEncoder rightEncoder, leftEncoder;
+    private final AHRS gyro;
+     public Drivetrain() {
+        super.register();
+        leftLeader = new CANSparkMax(RobotMap.leftLeaderID, MotorType.kBrushless);
+        rightLeader = new CANSparkMax(RobotMap.rightLeaderID, MotorType.kBrushless);
+        rightLeader.setInverted(true);
+        
+        leftFollower = new CANSparkMax(RobotMap.leftFollowerID, MotorType.kBrushless);
+        leftFollower.follow(leftLeader, false);
+        rightFollower = new CANSparkMax(RobotMap.rightFollowerID, MotorType.kBrushless);
+        rightFollower.follow(rightLeader, false);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+        leftLeader.setSmartCurrentLimit(80);
+        leftFollower.setSmartCurrentLimit(80);
+        rightLeader.setSmartCurrentLimit(80);
+        rightFollower.setSmartCurrentLimit(80);
+
+        leftEncoder = leftLeader.getEncoder();
+        rightEncoder = rightLeader.getEncoder();
+
+        leftEncoder.setPositionConversionFactor(Constants.conversionFactor);
+        rightEncoder.setPositionConversionFactor(Constants.conversionFactor);
+        gyro = new AHRS(Port.kMXP);
+    }
+    public void resetGyro() {
+        gyro.reset();    
+    }
+    public void setMotors(double left, double right){
+        leftLeader.set(left);
+        rightLeader.set(right);
+    }
+
+ 
+    public double getAngle() {
+        return gyro.getYaw();
+    }
+
+
+    public CANSparkMax getLeftMotor(){
+        return leftLeader;
+    }
+
+    public CANSparkMax getRightMotor(){
+        return rightLeader;
+    }
+    
+    public RelativeEncoder getLeftEncoder(){
+        return leftEncoder;
+    }
+
+    public RelativeEncoder getRightEncoder(){
+        return rightEncoder;
+    }
+
+    public double getLeftPosition(){
+        return leftEncoder.getPosition();
+    }
+
+    public double getRightPosition(){
+        return rightEncoder.getPosition();
+    }
+    public void resetEncoders() {
+        rightEncoder.setPosition(0);
+        leftEncoder.setPosition(0);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
 }
