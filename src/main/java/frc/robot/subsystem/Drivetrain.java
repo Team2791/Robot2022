8aba0e17a5178971ec59ac89d126cad48fecd1ba
frcteam2791 <team2791@gmail.com>
2022-11-21 19:44:06 -0500

    dtuff

--- src/main/java/frc/robot/subsystem/Drivetrain.java
+++ src/main/java/frc/robot/subsystem/Drivetrain.java
@@ -8,6 +8,7 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase{
@@ -81,6 +82,8 @@ public class Drivetrain extends SubsystemBase{
 
     @Override
     public void periodic() {
+		SmartDashboard.putNumber("Left position", getLeftPosition());
+		SmartDashboard.putNumber("Right position", getRightPosition());
 
     }
 }
